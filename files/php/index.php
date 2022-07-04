fb3c26c89e72ed4f866d1f96e62dc72ee6695a3a
Jessica <jessicadianefletcher@gmail.com>
2022-07-04 12:09:17 -0600

    Moved file locations and updated links- working on getting ready for php versions

---
+++ files/php/index.php
@@ -0,0 +1,82 @@
+<html>
+    <body>
+        <link rel="stylesheet" type="text/css" href="../">
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Jessica Fletcher Resume</title>
+    </body>
+    <header>
+        <?php
+            echo "<header>
+                <h1>Jessica<br>Fletcher</h1>
+                <nav class="header">
+                    <a href="index.php" title="Resume Home">Home</a>
+                    <a href="skills.php" title="Skills">Skills</a>
+                    <a href="portfolio.php" title="Resume Portfolio">Portfolio</a>
+                    <a href="about.php" title="About Jessica">About</a>
+                </nav>
+            </header>"
+            echo "<article>
+                <section>
+                    <h2>Personal Bio</h2>
+                        <p>I am in search of a web development career after completing my SLCC Web Development Certification. I'm hoping to improve through practice, experience, and working with others. My previous life experiences have taught me critical thinking, perseverance, and creativity. These skills will be an excellent foundation to help me succeed in the field of web development. </p>
+                        <p>I completed my major in Music despite deciding not to pursue a long-term career in Music. I felt it was more important for me to finish my degree before having kids so it would open doors for me in the future once I decided which direction to go.</p>
+                        <p>My professional experience is a little mismatched because I have gaps from becoming a mother after graduating from my university. I am working towards a career I will love, and web development feels like an excellent mix of my strengths in creativity, organization, and continual learning. </p>
+                </section>
+                <section>
+                    <h2>Education</h2>
+                        <dl>
+                            <dt><h3>Music Bachelor of Arts Degree:</h3></dt>
+                            <dd>Aug 2011- Dec 2014
+                                <ul>
+                                    <li>Brigham Young University</li>
+                                </ul>
+                            </dd>
+                        </dl>
+                </section>
+                <section>
+                    <h2>Professional Experience</h2>
+                        <dl>
+                            <dt><h3>Office Manager at Prattland Music Adventures:</h3></dt>
+                                <dd>March 2021- Present
+                                    <ul>
+                                        <li>Manage music lesson scheduling and billing</li>
+                                        <li>Contact potential and current clients through phone, text, and email</li>
+                                        <li>Work with music teachers to find substitutes or reschedule students</li>
+                                        <li>Maintain office cleanliness and restock needed supplies</li>
+                                        <li>Help resolve client concerns</li>
+                                        <li>Maintain management documents and spreadsheets</li>
+                                    </ul>
+                                </dd>
+                            <dt><h3>Teacher’s Aide:</h3></dt>
+                                <dd>February 2020- May 2021
+                                    <ul>
+                                        <li>Follow teacher instructions for lesson plans</li>
+                                        <li>Keep students focused on tasks</li>
+                                        <li>Maintain student safety at recess and in the classroom</li>
+                                        <li>Teach students to read, write, and do math</li>
+                                    </ul>
+                                </dd>
+                            <dt><h3>Etsy Store:</h3></dt>
+                                <dd>January 2016- Present
+                                    <ul>
+                                        <li>Sell custom crochet dolls</li>
+                                        <li>Discuss customer vision for desired product</li>
+                                        <li>Construct product and update customer to ensure it matches their vision</li>
+                                        <li>Ship product to customer</li>
+                                    </ul>
+                                </dd>
+                        </dl>
+                </section>
+            </article>"
+            echo "<footer>
+                <nav>
+                    <a href="index.html" title="Resume Home">Home</a>
+                    <a href="skills.html" title="Skills">Skills</a>
+                    <a href="portfolio.html" title="Resume Portfolio">Portfolio</a>
+                    <a href="about.html" title="About Jessica">About</a>
+                </nav>
+            </footer>"
+        ?>
+    </header>
+</html>
---
+++ files/php/practice.php
@@ -0,0 +1,25 @@
+<html>
+    <header>
+
+    </header>
+    <body>
+        <?php
+            $greeting = "Hello World!";
+            $paragraph = "What a fine day to be alive. :)";
+            $farewell = "...well farewell!";
+            echo $greeting . "<br>" . $paragraph . "<br>" . $farewell;
+            $x = 3;
+            $y = 10;
+            $z = $x + $y;
+            $a = $y - $x;
+            $b = $x * $y;
+            $c = $y / $x;
+            $d = $y % $x;
+            echo "<br>" . "$x + $y = $z";
+            echo "<br>" . "$y - $x = $a";
+            echo "<br>" . "$x * $y = $b";
+            echo "<br>" . "$y / $x = $c";
+            echo "<br>" . "$y % $x = $d";
+        ?>
+    </body>
+</html>
